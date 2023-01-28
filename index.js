8ca4b9c72231d8b30493d048271bab1722710514
MerikKuhn <mkuhn181@west-mec.org>
2023-01-27 18:20:39 -0700

    adding details and starting backend

---
+++ index.js
@@ -0,0 +1,9 @@
+let Nixon = document.getElementById('Nixonbtn');
+let McGovern = document.getElementById('McGovernbtn');
+Nixon.addEventListener('click', Nixon);
+McGovern.addEventListener('click', McGovern);
+function McGovern() {}
+function loadEvent() {
+  onload = alert('Please be wise on who you choose');
+}
+function Nixon() {}
