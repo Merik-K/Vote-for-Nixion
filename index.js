9d30724357bc3ca58671f0db54a072a5145ecb5b
MerikKuhn <mkuhn181@west-mec.org>
2023-01-31 18:19:44 -0700

    adding load events

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 let Nixon = document.getElementById('Nixonbtn');
 let McGovern = document.getElementById('McGovernbtn');
-Nixon.addEventListener('click', Nixon);
-McGovern.addEventListener('click', McGovern);
-function McGovern() {}
-function loadEvent() {
-  onload = alert('Please be wise on who you choose');
+var count = 0;
+let win = document.getElementById('winner');
+let lose = document.getElementById('loser');
+
+function doThis() {
+  alert('Please choose wisely on who to vote for!');
 }
-function Nixon() {}
+
+Nixon.addEventListener('click', NixonX);
+function NixonX() {
+  if (count == 0) {
+    win.style.display = 'initial';
+  }
+}
+
+McGovern.addEventListener('click', McGovernM);
+function McGovernM() {
+  if (count == 0) {
+    lose.style.display = 'initial';
+  }
+}
