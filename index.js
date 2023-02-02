51817984e73e7e15bffdf4827c8da5b7df1bdd0c
MerikKuhn <mkuhn181@west-mec.org>
2023-02-01 18:14:40 -0700

    more nixon work

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let McGovern = document.getElementById('McGovernbtn');
 var count = 0;
 let win = document.getElementById('winner');
 let lose = document.getElementById('loser');
+let yes = document.getElementById('si');
+let no = document.getElementById('no');
+let yess = 0;
+let noo = 0;
+let Nixonbtn2 = document.getElementById('Nixonbtn2');
 
 function doThis() {
   alert('Please choose wisely on who to vote for!');
@@ -19,5 +24,22 @@ McGovern.addEventListener('click', McGovernM);
 function McGovernM() {
   if (count == 0) {
     lose.style.display = 'initial';
+    yes.addEventListener('click', YesS);
+    no.addEventListener('click', NoO);
   }
+}
+function YesS() {
+  if (yess == 0) {
+    lose.style.display = 'none';
+  }
+  console.log('hello');
+  Nixonbtn2.style.display = 'initial';
+}
+
+function NoO() {
+  if (yess == 0) {
+    lose.style.display = 'none';
+  }
+  alert('Good Choice!');
+  console.log('hello');
 }
